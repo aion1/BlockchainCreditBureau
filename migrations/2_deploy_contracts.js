0e9c09d9a1464c956b3740440cb34e0c37c0e30c
egyptianf <walied2@protonmail.com>
2020-03-19 19:35:02 +0200

    minor updates

--- migrations/2_deploy_contracts.js
+++ migrations/2_deploy_contracts.js
@@ -6,6 +6,6 @@ const Accounts = artifacts.require("Accounts");
 module.exports = function(deployer) {
   deployer.deploy(Loan);
   deployer.deploy(Organization);
-  deployer.deploy(User);
   deployer.deploy(Accounts);
+  deployer.deploy(User);
 };
